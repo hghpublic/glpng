2266ea17d0df15619e6c6e21611aa7a34f5f6833
James Le Cuirot <chewi@aura-online.co.uk>
2025-05-25 14:59:09 +0100

    Bump version to 1.47

--- src/glpng.c
+++ src/glpng.c
@@ -1,5 +1,5 @@
 /*
- * PNG loader library for OpenGL v1.46 (02/06/14)
+ * PNG loader library for OpenGL v1.47 (25/05/2025)
  * by Ben Wyatt ben@wyatt100.freeserve.co.uk
  * Using LibPNG 1.0.2 and ZLib 1.1.3
  *
